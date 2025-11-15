68369babac6b0cb7c5bab453aee5ce50d6a25f74
omid187 <salehpouromid2261@gmail.com>
2025-11-15 02:35:40 -0800

    Add files via upload

---
+++ omid-main/index.php
@@ -0,0 +1,10 @@
+<?php
+include("index.html")
+?>
+
+تازه ها  
+<a href="anvaa.php">انواع</a>
+<a href="sign in.php">ثبت‌نام</a>
+<a href="darbare.php">درباره ما</a>
+
+<img src="mobile-accessories.jpg" alt="mobile accessories">
